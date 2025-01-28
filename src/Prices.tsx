b13f9ad10ca649dd75742a1459d9d42cd2764d4e
Aiden Peace <aidenpeace300@gmail.com>
2025-01-28 02:01:42 -0800

    added price text for each category

--- src/Prices.tsx
+++ src/Prices.tsx
@@ -9,18 +9,28 @@ export default function Prices() {
         <div className="photography">
           <h1 className="header_text">PHOTOGRAPHY</h1>
           <ul className="ul_content">
-            <li>Single Image: $35</li>
-            <li>5-Image Set: $175 ($35 per image)</li>
-            <li>10-Image Set: $350 ($35 per image)</li>
+            <li>Single Image: $25</li>
+            <li>5-Image Set: $100 ($20 per image)</li>
+            <li>10-Image Set: $175 ($17.50 per image)</li>
           </ul>
         </div>
         <div className="photography">
           <h1 className="header_text">VIDEOGRAPHY</h1>
+          <ul className="ul_content">
+            <li>15-Second Clip: $65</li>
+            <li>30-Second Clip: $120</li>
+            <li>1-Minute Highlight Reel: $230</li>
+          </ul>
         </div>
         <div className="photography">
           <h1 className="header_text">BUNDLE PACK</h1>
+          <ul className="ul_content">
+            <li>Photo & Video Combo (5 images + 15-second clip): $150</li>
+            <li>10 images + 45-second Clip Combo: $300</li>
+          </ul>
         </div>
       </div>
+      <h1 className="inspection_fee">STARTING AT $65 FOR LOCATION INSPECTION. TRAVEL FEES MAY APPLY.</h1>
     </div>
   );
 }
