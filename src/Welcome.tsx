20f523579a36845f78f705658469db1f1d9bb8e0
Aiden Peace <aidenpeace300@gmail.com>
2025-02-08 03:13:28 -0800

    ensuring that the user can not horizontally scroll

--- src/Welcome.tsx
+++ src/Welcome.tsx
@@ -6,10 +6,12 @@ export default function Welcome() {
   const gifRef = useRef<HTMLImageElement>(null);
 
   useEffect(() => {
-    if (gifRef.current) {
-      // Force the GIF to reload by appending a timestamp to the src
-      gifRef.current.src = `${landingPageDroneGif}?${new Date().getTime()}`;
-    }
+    const reloadGif = () => {
+      if (gifRef.current) {
+        gifRef.current.src = `${landingPageDroneGif}?${new Date().getTime()}`;
+      }
+    };
+    setTimeout(reloadGif, 100); // Delay by 100ms
   }, []);
 
   return (
@@ -20,23 +22,27 @@ export default function Welcome() {
         src={landingPageDroneGif}
         alt="loading..."
       />
-    <div className="text-container">
-      <ul class="dash">
-        <li className="overlay-intro">SOUTHERN CALIFORNIA CAPTURED DATA</li>
-      </ul>
-      <br />
-      <h3 className="overlay-heading">
-        EMBRACE THE <br /> LIMITLESS <br /> POTENTIAL OF <br /> DATA TECHNOLOGY
-      </h3>
-      <br />
-      <p className="overlay-outro"> With southern California reach, proprietary software and game-changing aerial imaging, Aero Altitude connects data-seekers to professional data-capturers. We stand for unbound progress – unlocking the full potential of drone technology and aerial data.</p>
-      <br />
-      <br />
-      <a className="cta" href="#contact">
-        <button className="contact-button">GET A QUOTE</button>
-      </a>
+      <div className="text-container">
+        <ul className="dash">
+          <li className="overlay-intro">SOUTHERN CALIFORNIA CAPTURED DATA</li>
+        </ul>
+        <br />
+        <h3 className="overlay-heading">
+          EMBRACE THE <br /> LIMITLESS <br /> POTENTIAL OF <br /> DATA TECHNOLOGY
+        </h3>
+        <br />
+        <p className="overlay-outro">
+          With southern California reach, proprietary software and game-changing
+          aerial imaging, Aero Altitude connects data-seekers to professional
+          data-capturers. We stand for unbound progress – unlocking the full
+          potential of drone technology and aerial data.
+        </p>
+        <br />
+        <br />
+        <a className="cta" href="#contact">
+          <button className="contact-button">GET A QUOTE</button>
+        </a>
+      </div>
     </div>
-
-  </div>
   );
 }
